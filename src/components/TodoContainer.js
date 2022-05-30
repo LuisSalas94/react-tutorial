690e37ebd5daa76e5e184b42b8efdd3e3248deca
Fernando <luisfernandosalasg@gmail.com>
2022-05-30 15:57:02 -0500

    Part6 done

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -7,7 +7,7 @@ import InputTodo from "./InputTodo";
 class TodoContainer extends Component {
 	state = {
 		todos: [
-			{
+			/* 	{
 				id: uuidv4(),
 				title: "Setup development environment",
 				completed: true,
@@ -21,9 +21,34 @@ class TodoContainer extends Component {
 				id: uuidv4(),
 				title: "Deploy to live server",
 				completed: false,
-			},
+			}, */
 		],
 	};
+
+	//Fetch Todos
+	/* 	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+			.then((res) => res.json())
+			.then((data) => this.setState({ todos: data }));
+	} */
+
+	//LocalStorage
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.todos !== this.state.todos) {
+			const temp = JSON.stringify(this.state.todos);
+			localStorage.setItem("todos", temp);
+		}
+	}
+
+	componentDidMount() {
+		const temp = localStorage.getItem("todos");
+		const loadedTodos = JSON.parse(temp);
+		if (loadedTodos) {
+			this.setState({
+				todos: loadedTodos,
+			});
+		}
+	}
 
 	handleChange = (id) => {
 		this.setState((prevState) => ({
