3a6f8e82713e307022174179035861a5d298f6c7
Fernando <luisfernandosalasg@gmail.com>
2022-05-30 14:12:24 -0500

    Part2 done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,14 @@ class TodoList extends Component {
 		return (
 			<ul>
 				{this.props.todos.map((todo) => {
-					return <TodoItem key={todo.id} todo={todo} />;
+					return (
+						<TodoItem
+							key={todo.id}
+							todo={todo}
+							handleChangeProps={this.props.handleChangeProps}
+							deleteTodoProps={this.props.deleteTodoProps}
+						/>
+					);
 				})}
 			</ul>
 		);
